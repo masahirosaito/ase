4f1c51be6948dfe9b509199d64dffad801a13c50
James Kermode <james.kermode@gmail.com>
2020-10-29 10:02:05 +0000

    simplify test_smallcell.py

--- ase/test/precon/test_smallcell.py
+++ ase/test/precon/test_smallcell.py
@@ -1,32 +1,12 @@
 import pytest
-from ase.build import bulk
-from ase.calculators.emt import EMT
+from ase.atoms import Atoms
 from ase.optimize.precon import PreconLBFGS
 
-import warnings
+
+def test_precon_warn():
+    with pytest.warns(UserWarning, match='The system is likely too small'):
+        PreconLBFGS(Atoms('H'))
 
 
-@pytest.mark.slow
-@pytest.mark.parametrize('N', [1, 3])
-def test_precon(N):
-    a0 = bulk('Cu', cubic=True)
-    a0 *= (N, N, N)
-
-    # perturb the atoms
-    s = a0.get_scaled_positions()
-    s[:, 0] *= 0.995
-    a0.set_scaled_positions(s)
-
-    atoms = a0.copy()
-    atoms.calc = EMT()
-
-    # check we get a warning about small system
-    with warnings.catch_warnings(record=True) as w:
-        # Cause all warnings to always be triggered.
-        warnings.simplefilter("always")
-        PreconLBFGS(atoms, precon="auto")
-        if N == 1:
-            assert len(w) == 1
-            assert "The system is likely too small" in str(w[-1].message)
-        else:
-            assert len(w) == 0
+def test_precon_nowarn():
+    PreconLBFGS(Atoms('100H'))
